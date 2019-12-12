de9a60245743761e6905e241bd8bf29d7debf3c1
JavierClairvaux <javcabr@gmail.com>
2019-12-12 13:50:27 -0800

    deleted unnecessary comments

--- scripts/server.sh
+++ scripts/server.sh
@@ -68,17 +68,6 @@ server=/consul/127.0.0.1#8600
 
 sudo systemctl restart dnsmasq
 
-# #Install influxdb
-# sudo curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
-# source /etc/lsb-release
-# echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
-# sudo apt-get update
-# sudo apt-get install influxdb -y
-# sudo systemctl start influxd
-# sudo apt install influxdb-client
-# influx -execute "create database telegraf
-# create user telegraf with password 'pass'"
-#
 #Install grafana
 cd /home/vagrant
 sudo apt-get install -y software-properties-common
